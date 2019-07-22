310fb90cad67a0f9511622b1d8b168fb0119eac7
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 16:48:01 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,8 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet"
+        age: "50"
         },
       :matriarch => {
         #your key/value pairs here
