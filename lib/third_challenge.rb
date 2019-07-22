95b7cd83b9ee0d87756fab169ae09e520cd0396f
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 16:55:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,11 +14,7 @@ def third_challenge
         age: "15"
         status: "alive"
         },
-      hero_friends: 
-      [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
-      ]
+      hero_friends: []
    }, 
    capulet: {
       patriarch: {
