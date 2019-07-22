57d2f141abc346bf02d4ab5e95767d44c9f2e7d7
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 16:50:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,32 +5,32 @@ def third_challenge
         name: "Lord Montague"
         age: "53"
         },
-      :matriarch => {
+      matriarch: = {
         name: "Lady Montague"
         age: "54"
         },
-      :hero => {
+        hero: {
         name: "Romeo"
         age: "15"
         status: "alive"
         },
-      :hero_friends => []
+      hero_friends: []
    }, 
-   :capulet => {
-      :patriarch => {
+   capulet: {
+      patriarch: {
         name: "Lord Capulet"
         age: "50"
         },
-      :matriarch => {
+      matriarch: {
         name: "Lady Capulet"
         age: "51"
         },
-      :heroine => {
+      heroine: {
         name: "Juliet"
         age: "15"
         status: "alive"
         },
-      :heroine_friends => []
+      heroine_friends: []
    }
   }
 
