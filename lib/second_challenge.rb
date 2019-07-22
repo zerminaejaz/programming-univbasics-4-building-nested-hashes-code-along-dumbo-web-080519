b077a04a95d85b0ed2141984b8ba13f0e606395c
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 16:40:00 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,15 @@
 def second_challenge
   epic_tragedy = {
     montague: = {
-     patriarch: {}
-     matriarch: {}
-     heroine: {}
+     patriarch: {},
+     matriarch: {},
+     heroine: {},
      heroine_friends: []
       }, 
     capulet: = {
-     patriarch: {}
-     matriarch: {}
-     heroine: {}
+     patriarch: {},
+     matriarch: {},
+     heroine: {},
      heroine_friends: []
       }
   }
