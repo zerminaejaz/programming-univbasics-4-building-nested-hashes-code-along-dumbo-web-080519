d2f069e4339c03ec321bbd3f22af35028f28dd21
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 17:50:52 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,19 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-     patriarch: {}
-     matriarch: {}
-     heroine: {}
-     heroine_friends: []
-      }, 
-    :capulet => {
-     patriarch: {}
-     matriarch: {}
-     heroine: {}
-     heroine_friends: []
-      }}
-
-  
+  epic_tragedy = 
+  {
+   :montague => 
+   {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => 
+   {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+  }
 end
