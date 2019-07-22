383e77f554f8a6911d5c92f6e0fc03ae1657fc6f
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 17:00:04 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -17,7 +17,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"}
-        {name: "MNurse", age: "44", attitude: "worried"}
+        {name: "Nurse", age: "44", attitude: "worried"}
 
       ]
    }
