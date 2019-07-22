c1fbc092c8ffe1dc98ebbb88ce2b722773268922
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 16:54:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -34,7 +34,11 @@ def third_challenge
         age: "15"
         status: "alive"
         },
-      heroine_friends: []
+      heroine_friends:
+      [
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "hot-headed"}
+      ]
    }
   }
 
