1af6716a6459ab3c6d873f5afbc598a0f8ad93fd
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 17:00:44 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,7 +15,8 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => 
+      [
         {name: "Steven", age: "30", attitude: "confused"}
         {name: "Nurse", age: "44", attitude: "worried"}
       ]
